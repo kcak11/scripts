d1d28dafe719f9f17088408323900282e0c3488c
K.C.Ashish Kumar <kcak11@gmail.com>
2017-11-02 18:53:17 +0530

    Create clean_nav_outline.js

---
+++ clean_nav_outline.js
@@ -0,0 +1,32 @@
+/*
+ * Open the navigation outline in a new tab in Chrome
+ * then run this script from dev-toolbar
+ * */
+(function() {
+    var plinks = document.querySelectorAll("[if=\"options.properties\"]");
+    for (var i = 0; i < plinks.length; i++) {
+        (function(p) {
+            setTimeout(function() {
+                try {
+                    var href = p.getAttribute("href");
+                    var ifr = document.createElement("iframe");
+                    ifr.style.display = "none";
+                    document.body.appendChild(ifr);
+                    ifr.src = href;
+                    ifr.onload = function() {
+                        var d = ifr.contentWindow.document;
+                        var propForm = d.querySelector("form[action*=propertiesView]");
+                        var linkText = propForm.querySelector("ul[if=\"subject.content.cards.hyperlinks\"] a span");
+                        if (linkText) {
+                            var nameField = propForm.querySelector("[name=\"content[displayName]\"]");
+                            if (nameField && nameField.value !== linkText.innerHTML) {
+                                nameField.value = linkText.innerHTML;
+                                propForm.submit();
+                            }
+                        }
+                    };
+                } catch (exjs) {}
+            }, (i * 500));
+        }(plinks[i]), i);
+    }
+}());
